bfcc9407931d488287a66e787a942fc24a4e3657
alireza200411 <alirezamoradi292048@gmail.com>
2025-07-07 18:11:55 +0330

    add class based view for error and add confitional for ananymous user in register_instructor view

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'account'
 urlpatterns = [
-    path('error_not_access', views.error_not_access, name='not_access'),
+    path('error_not_access', views.ErrorNotAccessPage.as_view(), name='not_access'),
     path('sign_up_instructor', views.register_instructor, name='signup'),
     path('profile/<str:username>', views.profile_instructor, name='profile')
 ]
--- account/views.py
+++ account/views.py
@@ -1,32 +1,39 @@
 from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+
 from .models import MyUser, InstructorProfile
 from .forms import InstructorForm
 
 
+
 def register_instructor(request):
     user = request.user
-    instructor = MyUser.objects.get(id=user.id)
-    if InstructorProfile.objects.filter(name__username=request.user.username).exists():
-        return redirect('account:profile', username=request.user.username)
-    else:
-        if instructor.is_instructor:
-            if request.method == "POST":
-                form = InstructorForm(request.POST, user=request.user)
-                if form.is_valid():
-                    instructor_main = form.save(commit=False)
-                    instructor_main.name = request.user
-                    instructor_main.save()
-                    form.save_m2m()
-                    return redirect('account:profile', username=request.user.username)
-            else:
-                form = InstructorForm(user=request.user)
+    if user.is_authenticated:
+        instructor = MyUser.objects.get(id=user.id)
+        if InstructorProfile.objects.filter(name__username=request.user.username).exists():
+            return redirect('account:profile', username=request.user.username)
         else:
-            return redirect('account:not_access')
+            if instructor.is_instructor:
+                if request.method == "POST":
+                    form = InstructorForm(request.POST, user=request.user)
+                    if form.is_valid():
+                        instructor_main = form.save(commit=False)
+                        instructor_main.name = request.user
+                        instructor_main.save()
+                        form.save_m2m()
+                        return redirect('account:profile', username=request.user.username)
+                else:
+                    form = InstructorForm(user=request.user)
+            else:
+                return redirect('account:not_access')
+    else:
+        return redirect('account:not_access')
     return render(request, 'account/register_instructor.html', context={'form': form})
 
 
-def error_not_access(request):
-    return render(request, 'account/not_access.html', context={})
+class ErrorNotAccessPage(TemplateView):
+    template_name = 'account/not_access.html'
+
 
 
 def profile_instructor(request, username):
