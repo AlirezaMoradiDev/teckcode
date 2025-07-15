0831ef0d16b28987a23d24133cce58746ec1a898
alireza200411 <alirezamoradi292048@gmail.com>
2025-07-15 22:32:27 +0330

    add Course model and Category model

--- course/models.py
+++ course/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+import datetime
+from django.utils.text import slugify
+from account.models import InstructorProfile, MyUser
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    descriptions = models.TextField()
+    created = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=75)
+    descriptions = models.TextField()
+    start_time = models.DateField(default=datetime.date.today())
+    end_time = models.DateField()
+    capacity = models.IntegerField()
+    instructor = models.ForeignKey(InstructorProfile, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    student = models.ManyToManyField(MyUser)
+    slug = models.SlugField(null=True, blank=True)
+    view = models.IntegerField(default=0)
+    created_at = models.DateField()
+    is_active = models.BooleanField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super().save()
+
+    def __str__(self):
+        return self.title
